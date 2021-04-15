9970888486c6fef11195ea1487ad2ed61860dda8
marcelinazoto <marcelina.zoto.villca2109@gmail.com>
2021-04-15 18:39:09 -0400

    problem2

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
 //****************Ejemplo 1****************
-var num1: number = 2;
-var num2: number = 21;
-var num3: number = 83;
-function primo(n: number): boolean
+var cad1: string = 'this is a sample string';
+var cad2: string = 'a';
+var cad3: string = 'bang';
+
+function cadenaIvertida(texto: string) 
 {
-    for(let i : number = 2; i < n; i++)
-    {
-        if(n % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
+  return texto.split('').reverse().join('');
 }
-console.log(primo(num1));
-console.log(primo(num2));
-console.log(primo(num3));
+console.log(cadenaIvertida(cad1));
+console.log(cadenaIvertida(cad2));
+console.log(cadenaIvertida(cad3));
