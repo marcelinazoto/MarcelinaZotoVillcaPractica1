bfda177897d0e269decbdb655b2d476c84c0f3d3
marcelinazoto <marcelina.zoto.villca2109@gmail.com>
2021-04-15 22:19:55 -0400

    problem7

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,32 @@
-//****************Ejemplo 8****************
-var c = 9;
-var j = 2;
-var numerosPrimos = [];
-for (; j < c; j++) {
+//****************Ejemplo 7****************
+let cad1:String="abca";
+let cad2:String="xyzbac";
+let vector=new Array();
+let contador=0;
+let posicion=-1;
+for(let i=0;i<cad1.length;i++){
+    for(let j=0;j<cad2.length;j++){
+        if(cad1[i]==cad2[j]){
+           if( existe(j)!=true){
+            contador++;
+            insertar(j);
+            break;
+           }
+        }
 
-  if (primo(j)) {
-    numerosPrimos.push(j);
-  }
-  
-}
+        }
 
-console.log(numerosPrimos);
+    }
+    console.log(contador);
 
-function primo(n: number): Boolean
-{
-    for(let i : number = 2; i < n; i++)
-    {
-        if(n % i === 0)
-        {
-            return false;
+function existe(x: number){
+    for(let i=0;i<vector.length;i++){
+        if(vector[i]==x){
+            return true;
         }
     }
-    return true;
+
+}
+function insertar(result: number){
+    vector[vector.length]=result;
 }
